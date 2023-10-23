bc523108af614bfb0aa7501cf4fd809527f2c6db
munhof <munhofdev@gmail.com>
2023-10-22 21:57:19 -0300

    actualizado ABB

--- AVL/estudiantes/src/main/java/aed/ABB.java
+++ AVL/estudiantes/src/main/java/aed/ABB.java
@@ -34,12 +34,26 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
     }
 
     public T minimo() {
-            throw new UnsupportedOperationException("No implementada aun");
-    }
+        Nodo actual = _Raiz;
+        T min = null;
+        while(actual.izq != null){
+            actual = actual.izq;
+        }
+        min = actual.valor; 
+        return min;
+        }
+       
+
 
     public T maximo() {
-            throw new UnsupportedOperationException("No implementada aun");
-    }
+        Nodo actual = _Raiz;
+        T min = null;
+        while(actual.der != null){
+            actual = actual.der;
+        }
+        min = actual.valor; 
+        return min;
+        }
 
     public void insertar(T elem) {
         Nodo actual = _Raiz;
@@ -49,70 +63,149 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
             _Cardinal = 1;
             return;
         } 
-        
-        if (pertenece(elem)) {
-            return;
-        }
-
-        Nodo hijoMayor = actual.der;
-        Nodo hijoMenor = actual.izq;
-        Nodo padre = null;
-        while (hijoMenor != null || hijoMayor != null) {              
-            padre = actual;
-            hijoMenor = actual.izq;
-            hijoMayor = actual.der;
-            if (actual.valor.compareTo(elem) > 0) {                    
-                actual = hijoMenor;
+        Nodo anterior = null;
+        while (actual != null) {
+            anterior = actual;
+            if (elem.compareTo(actual.valor) < 0) {
+                actual = actual.izq;
+            } else if (elem.compareTo(actual.valor) > 0) {
+                actual = actual.der;
             } else {
-                actual = hijoMayor;
+                // Elemento ya existe en el árbol, no hacemos nada
+                return;
             }
         }
-        if (actual.valor.compareTo(elem) > 0) {                    
-                actual.izq = nuevo;
-            } else {
-                actual.der = nuevo;
-            }
-        _Cardinal ++;
+        if (elem.compareTo(anterior.valor) < 0) {
+            anterior.izq = nuevo;
+        } else {
+            anterior.der = nuevo;
+        }
+        nuevo.padre = anterior;
+        _Cardinal++;
     }
+    
 
     public boolean pertenece(T elem) {
-        Nodo actual = _Raiz;
-        while (actual != null) {
-            if (actual.valor.compareTo(elem) == 0) {
-                return true;
-            } else {
-                if (actual.valor.compareTo(elem) < 0) {
-                    actual = actual.der;
-                } else {
-                    actual = actual.izq;
-                }
-            }
+        return buscarElemento(_Raiz, elem);
+    }
+    
+    private boolean buscarElemento(Nodo nodo, T elem) {
+        if (nodo == null) {
+            return false; // El elemento no se encontró en el árbol
         }
-        return false;
+    
+        int comparacion = elem.compareTo(nodo.valor);
+        if (comparacion < 0) {
+            return buscarElemento(nodo.izq, elem);
+        } else if (comparacion > 0) {
+            return buscarElemento(nodo.der, elem);
+        } else {
+            return true; // Elemento encontrado en el árbol
+        }
     }
 
     public void eliminar(T elem) {
-        throw new UnsupportedOperationException("No implementada aun");
+        Nodo resultado = eliminarRecursivo(_Raiz, elem);
+        if (resultado != null) {
+            _Raiz = resultado; // Actualizamos la raíz si ha cambiado
+            _Cardinal--;
+        }
     }
+    
+    private Nodo eliminarRecursivo(Nodo raiz, T elem) {
+        if (raiz == null) {
+            return raiz; // El elemento no se encontró en el árbol
+        }
+    
+        if (elem.compareTo(raiz.valor) < 0) {
+            raiz.izq = eliminarRecursivo(raiz.izq, elem);
+        } else if (elem.compareTo(raiz.valor) > 0) {
+            raiz.der = eliminarRecursivo(raiz.der, elem);
+        } else {
+            // Este es el nodo que queremos eliminar
+            if (raiz.izq == null) {
+                return raiz.der; // Caso 1: Nodo con un solo hijo o ningún hijo
+            } else if (raiz.der == null) {
+                return raiz.izq; // Caso 1: Nodo con un solo hijo o ningún hijo
+            }
+            // Caso 2: Nodo con dos hijos, obtenemos el sucesor inorden (nodo más pequeño en el subárbol derecho)
+            raiz.valor = minimoValor(raiz.der);
+            raiz.der = eliminarRecursivo(raiz.der, raiz.valor);
+        }
+        return raiz;
+    }
+    
+    private T minimoValor(Nodo nodo) {
+        T minValor = nodo.valor;
+        while (nodo.izq != null) {
+            minValor = nodo.izq.valor;
+            nodo = nodo.izq;
+        }
+        return minValor;
+    }
+    
 
     public String toString() {
-        throw new UnsupportedOperationException("No implementada aun");
+        StringBuilder result = new StringBuilder();
+        result.append("{");
+        imprimirInOrden(_Raiz, result);
+        if (result.length() > 1) {
+            result.deleteCharAt(result.length() - 1); // Eliminar la última coma si hay elementos
+        }
+        result.append("}");
+        return result.toString();
     }
+    
+    private void imprimirInOrden(Nodo nodo, StringBuilder result) {
+        if (nodo == null) {
+            return;
+        }
+        imprimirInOrden(nodo.izq, result);
+        result.append(nodo.valor);
+        result.append(",");
+        imprimirInOrden(nodo.der, result);
+    }
+    
 
     private class ABB_Iterador implements Iterador<T> {
         private Nodo _actual;
-
-        public boolean haySiguiente() {
-            throw new UnsupportedOperationException("No implementada aun");
+        private Stack<Nodo> pila; // Usaremos una pila para realizar el recorrido en inorden
+    
+        public ABB_Iterador() {
+            _actual = _Raiz;
+            pila = new Stack<>();
+            while (_actual != null) {
+                pila.push(_actual);
+                _actual = _actual.izq;
+            }
         }
-
+    
+        public boolean haySiguiente() {
+            return !pila.isEmpty();
+        }
+    
         public T siguiente() {
-            throw new UnsupportedOperationException("No implementada aun");
+            if (!haySiguiente()) {
+                throw new NoSuchElementException("No hay más elementos para recorrer");
+            }
+    
+            _actual = pila.pop();
+            T valor = _actual.valor;
+    
+            // Avanzamos al siguiente nodo en inorden (subárbol derecho si existe)
+            _actual = _actual.der;
+            while (_actual != null) {
+                pila.push(_actual);
+                _actual = _actual.izq;
+            }
+    
+            return valor;
         }
     }
-
+    
     public Iterador<T> iterador() {
         return new ABB_Iterador();
     }
+    
 
 }
