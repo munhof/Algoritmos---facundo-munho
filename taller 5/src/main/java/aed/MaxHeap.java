0678969b5a283210c349dfccf248e1ffc4b45c69
munhof <109393692+munhof@users.noreply.github.com>
2023-11-17 21:57:06 -0300

    Add files via upload

---
+++ taller 5/src/main/java/aed/MaxHeap.java
@@ -0,0 +1,15 @@
+package aed;
+
+public class MaxHeap {
+
+    public MaxHeap(int length) {
+    }
+
+    public void insert(Router router, int trafico) {
+    }
+
+    public Router extractMax() {
+        return null;
+    }
+
+}
